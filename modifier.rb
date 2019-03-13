63dcec83f38384dd6b0e5ab6d37be0c3782885f6
Oscar Gomez <oigomezz@unal.edu.co>
2019-03-13 10:33:35 -0500

    Methods

---
+++ modifier.rb
@@ -0,0 +1,39 @@
+class Modifier
+
+	def add_money_account
+
+	end
+
+	def withdraw_money
+		
+	end
+
+	def send_money
+		
+	end
+	
+	def add_money_mattress
+		
+	end
+	
+	def withdraw_money_mattress
+		
+	end
+	
+	def add_money_pocket
+
+	end
+
+	def withdraw_money_pocket
+		
+	end
+
+	def send_money_pocket
+
+	end
+
+	def add_money_goal
+		
+	end
+	
+end
