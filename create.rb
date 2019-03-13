a7a3a4eebaf45fcca625f2f61837b7217442aa4f
yocuervoc <yocuervoc@unal.edu.co>
2019-03-13 14:22:44 -0500

    login, create pockets

--- create.rb
+++ create.rb
@@ -1,38 +1,59 @@
 require_relative 'connection.rb'
 require_relative 'user.rb'
+require_relative 'login.rb'
 
 class Creation
 
-	def initialize
+	def initialize(user_name, password, email)
+		@user_name= user_name
+		@password = password
+		@email = email
 		@db_connection = DbConnection.new()
 	end
-	
-	def register_user(name, password, email)
-		current_user = User.new( name, password, email)
+
+	def register_user()
+		current_user = User.new(@user_name, @password, @email)
 		succeful_access = @db_connection.client.query("INSERT INTO accounts (disponible,mattress, total) VALUES (0,0,0);", :symbolize_keys => true)
 		result = @db_connection.client.query("select id from accounts order by id desc limit 1;", :symbolize_keys => true)
+		id_count=999999
 		result.each do |row|
-			puts row
+			id_count= row[:id]
 		end
+
+		succeful_access = @db_connection.client.query("insert into users(mail, password, accounts_id ) values (\"#{current_user.email}\",\"#{ current_user.password}\", #{id_count});", :symbolize_keys => true)
+
 	end
 
-	def create_pocketspassword
+	def create_pocket(pocket_name)
+		#insert into pockets(pocketMoney, name, accounts_id ) values (0, 'NOMBRE', 3);
+
+		sesion=Login.new(@email, @password)
+		id_user = sesion.log
+
+		result = @db_connection.client.query("select accounts_id from users where id = #{id_user} ;", :symbolize_keys => true)
+		accounts_id=999999
+		result.each do |row|
+			accounts_id=row[:accounts_id]
+		end
+
+
+		succeful_access = @db_connection.client.query("insert into pockets(pocketMoney, name, accounts_id ) values (0.0, \"#{pocket_name}\",  #{accounts_id});", :symbolize_keys => true)
 
 	end
 
 	def delete_pockets
-		
+
 	end
 
 	def create_goals
-		
+
 	end
 
 	def delete_goals
-		
+
 	end
 
 end
 
-test = Creation.new()
-test.register_user("oscar", "password", "iasj@gmail.com")
+test = Creation.new("yocc", "password3", "yocc@gmail.com")
+test.create_pocket("carro")
--- user.rb
+++ user.rb
@@ -1,10 +1,13 @@
+
 class User
-  attr_accessor :name, :password, :email 
+  attr_accessor :name, :password, :email
 
   def initialize( name, password, email)
   	@name = name
   	@password = password
   	@email = email
+    @db_connection = DbConnection.new()
   end
+
 
 end
